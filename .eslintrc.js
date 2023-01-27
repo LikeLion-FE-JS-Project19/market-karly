6595e33fee254286ed43477c2d0c622d9f510755
senasoon <senasoon0@gmail.com>
2023-01-27 20:49:24 +0900

    [🚨setting] eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
