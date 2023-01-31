601434c89b22295ea8741fde05f1e6472da3a9bb
Juhee-Hwang <julia0276@naver.com>
2023-02-01 01:21:05 +0900

    [:sparkles: feat] 장바구니 버튼 함수 구현 #26

--- client/js/juhee.js
+++ client/js/juhee.js
@@ -39,3 +39,15 @@ export function swiper3() {
 	},
 });
 }
+
+export function getNodes(node){
+  if(typeof node !== 'string'){
+    throw new Error('getNode 함수의 인자는 문자 타입 이여야 합니다.');
+  }
+  return document.querySelectorAll(node)
+}
+
+export function onClickAddCartHandler(e){
+  e.preventDefault();
+	console.log('장바구니 담기 완료~')
+}
--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,9 @@
-import { swiper2, swiper3 } from "./juhee.js";
+import { swiper2, swiper3, onClickAddCartHandler, getNodes } from "./juhee.js";
 
 window.addEventListener('DOMContentLoaded', swiper2);
 window.addEventListener('DOMContentLoaded', swiper3);
+
+const addCartButtonList = getNodes('.btn-add-cart');
+addCartButtonList.forEach((el) => {
+  el.addEventListener('click',onClickAddCartHandler);
+})
