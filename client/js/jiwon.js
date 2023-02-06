2fe105b8c640741da5ec8de06c39198f9ebe2d4b
no-support <tk2ektk3ek@gmail.com>
2023-02-07 01:55:29 +0900

    [🔨 refactor] 문의글 등록 기능 구현, 키보드 접근성 개선 #61

--- client/js/jiwon.js
+++ client/js/jiwon.js
@@ -308,7 +308,7 @@ function createNoticeTr(data) {
     answerDatetime,
   } = data;
   return /* html */ `
-  <tr class="qna__item--notice" data-kind="public" data-id="${id}">
+  <tr class="qna__item--notice" data-kind="public" data-id="${id}" role="button" aria-expanded="false" tabindex="0">
     <td>
       ${title}
     </td>
@@ -354,7 +354,7 @@ function createPrivateTr(data) {
   const answerStatusClass = answerDatetime ? 'qna__item--complete' : '';
   const answerStatus = answerDatetime ? '답변완료' : '답변대기';
   return /* html */ `
-  <tr class="qna__item--private ${answerStatusClass}" data-kind="private" data-id="${id}">
+  <tr class="qna__item--private ${answerStatusClass}" data-kind="private" data-id="${id}" role="button" aria-expanded="false"  tabindex="0">
     <td>비밀글입니다.</td>
     <td>${maskingName(writer)}</td>
     <td>${questionDatetime}</td>
@@ -377,7 +377,7 @@ function createPublicTr(data) {
   const answerStatusClass = answerDatetime ? 'qna__item--complete' : '';
   const answerStatus = answerDatetime ? '답변완료' : '답변대기';
   return /* html */ `
-  <tr class="${answerStatusClass}" data-kind="public" data-id="${id}">
+  <tr class="${answerStatusClass}" data-kind="public" data-id="${id}" role="button" aria-expanded="false" tabindex="0">
     <td>${title}</td>
     <td>${maskingName(writer)}</td>
     <td>${questionDatetime}</td>
@@ -397,10 +397,18 @@ export function toggleContent(qnas) {
     } else if (tr.dataset.kind === 'public') {
       const content = document.querySelector('.content');
       if (content) {
-        content.remove();
-        return;
+        if (content.previousElementSibling.dataset.id === tr.dataset.id) {
+          // 클릭하여 열려는 행이 이미 열려있는 행과 같으면 닫고 끝난다(토글).
+          content.previousElementSibling.ariaExpanded = false;
+          content.remove();
+          return;
+        } else {
+          content.previousElementSibling.ariaExpanded = false;
+          content.remove();
+        }
       }
       const qna = qnas.find((item) => item.id === Number(tr.dataset.id));
+      tr.ariaExpanded = true;
       tr.insertAdjacentHTML('afterend', createContentTr(qna));
     }
   });
@@ -509,7 +517,7 @@ export function renderModal(data) {
   });
 }
 
-function createModal({ name, image } = data) {
+function createModal({ name, image, id } = data) {
   return /* html */ `
   <div class="qna__modal-container hidden">
     <div class="qna__modal">
@@ -519,7 +527,7 @@ function createModal({ name, image } = data) {
       </div>
       <div class="qna__modal-product">
         <img src="./assets/product/${image.thumbnail}" alt="${image.alt}">
-        <span>${name}</span>
+        <span data-product-id="${id}" id="qna__modal-product-id">${name}</span>
       </div>
       <div class="qna__modal-form-container">
         <div class="qna__modal-form-title-container">
@@ -578,7 +586,7 @@ function createModal({ name, image } = data) {
         </div>
 
         <div class="qna__modal-form-private-container">
-          <input type="checkbox" name="qna__modal-form-private" id="qna__modal-form-private">
+          <input type="checkbox" name="qna__modal-form-private" id="qna__modal-form-private" class="a11yHidden">
           <label for="qna__modal-form-private">비밀글로 문의하기</label>
         </div>
       </div>
@@ -591,9 +599,50 @@ function createModal({ name, image } = data) {
   `;
 }
 
-export function qnaModalSubmitHandler(event) {
-  const qnaModalFormTitle = document.querySelector('#qna__modal-form-title');
+export async function qnaModalSubmitHandler(event) {
+  const qnaModalProductId = document.querySelector('#qna__modal-product-id')
+    ?.dataset.productId;
+  const qnaModalFormTitle = document.querySelector(
+    '#qna__modal-form-title'
+  )?.value;
   const qnaModalFormContents = document.querySelector(
     '#qna__modal-form-contents'
-  );
+  )?.value;
+  const qnaModaFormPrivate = document.querySelector('#qna__modal-form-private')
+    ?.checked
+    ? 'private'
+    : 'public';
+  const now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  month = month < 10 ? '0' + month : month;
+  let date = now.getDate();
+  date = date < 10 ? '0' + date : date;
+  const questionDatetime = `${year}${month}${date}`;
+
+  if (
+    qnaModalFormTitle ||
+    qnaModalFormContents ||
+    qnaModalFormTitle.trim() ||
+    qnaModalFormContents.trim()
+  ) {
+    alert('제목과 내용을 입력해주세요.');
+  } else {
+    const response = await fetch('http://localhost:3001/QnAs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        id: '',
+        type: qnaModaFormPrivate,
+        product: qnaModalProductId,
+        title: qnaModalFormTitle,
+        writer: '홍길동',
+        contents: qnaModalFormContents,
+        questionDatetime: questionDatetime,
+      }),
+    });
+    const result = await response.json();
+  }
 }
